dda0bb4815f252a04824f0bcf98670290bdf13e8
Danny Brandt <dvbrandt90@gmail.com>
2020-12-14 05:13:14 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,9 +1,9 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
 your_hash ={
-  name => "Danny",
-  person_type => "boy type",
-  common_trait => "nose"
+  "name" => "Danny",
+  "person_type" => "boy type",
+  "common_trait" => "nose"
 }
   
 end
